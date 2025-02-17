b26f53e4f63f05836037da4990d0f8152c281098
Chain Chonchanok Wongaphai <chonchanok@nipa.cloud>
2025-02-17 19:11:43 +0700

    add noto

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,78 @@
-import express from 'express';
-import path from 'path';
-import livereload from 'livereload';
-import connectLivereload from 'connect-livereload';
+import express from "express";
+import path from "path";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
 import {
   MessageGatewayService,
   Channel,
-} from './message_gateway/MessageGatewayService';
+} from "./message_gateway/MessageGatewayService";
 
 const app = express();
 const port = 3030;
 
 // สร้าง livereload server และกำหนดให้เฝ้าดูโฟลเดอร์ views
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'views'));
+liveReloadServer.watch(path.join(__dirname, "views"));
 
 // เมื่อ livereload server เริ่มเชื่อมต่อ ให้รีเฟรชเบราว์เซอร์
-liveReloadServer.server.once('connection', () => {
+liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
-    liveReloadServer.refresh('/');
+    liveReloadServer.refresh("/");
   }, 100);
 });
 
 app.use(connectLivereload());
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    logo: 'https://space.nipa.cloud/assets/images/Logo-space.png',
-    recipient_name: 'John Doe',
-    billing_period: '01/JAN/25 - 01/FEB/25',
-    project_id: 'IDW985W6-3W73-11WW-WW56-0242WW120002',
+app.get("/", (req, res) => {
+  res.render("index", {
+    logo: "https://space.nipa.cloud/assets/images/Logo-space.png",
+    recipient_name: "John Doe",
+    wallet_type: "POSTPAID",
+    cycle_started_at: "01 JAN 25",
+    cycle_end_at: "31 FEB 25",
+    project_id: "IDW985W6-3W73-11WW-WW56-0242WW120002",
+    project_name: "test_project",
+    total_price: 2250000000,
+    vat: 2000000,
+    grand_total: 245000,
+    outstanding_balance: 30000000,
     resources: [
-      { name: 'Compute Instance', price: '100,000.00 THB' },
-      { name: 'Volumes', price: '50,000.00 THB' },
+      { name: "Compute Instance", price: 100000000 },
+      { name: "Volumes", price: 50000000 },
     ],
-    total_price: '225,000.00 THB',
-    vat: '15,750.00 THB',
-    grand_total: '240,750.00 THB',
-    // customer_support_phone: "08 6328 3030",
-    // support_email: "support@nipa.cloud",
   });
 });
 
 const messageGatewayService = new MessageGatewayService();
 
-app.get('/send', async (req, res) => {
+app.get("/send", async (req, res) => {
   try {
     const body = {
-      template: 'POSTPAID_CYCLE_REPORT',
-      module: '(dev) Nipa Cloud Space',
+      template: "WALLET_CYCLE_USAGE_REPORT",
+      module: "(dev) Nipa Cloud Space",
       targets: {
-        [Channel.emails]: ['chonchanok@nipa.cloud'],
+        [Channel.emails]: ["chonchanok@nipa.cloud, c.wongaphai@gmail.com"],
       },
       data: {
-        logo: 'https://space.nipa.cloud/assets/images/Logo-space.png',
-        recipient_name: 'John Doe',
-        billing_period: '01/JAN/25 - 01/FEB/25',
-        project_id: 'IDW985W6-3W73-11WW-WW56-0242WW120002',
+        subject: `Cycle Report - 01 JAN 25 - 31 FEB 25`,
+        logo: "https://space.nipa.cloud/assets/images/Logo-space.png",
+        recipient_name: "John Doe",
+        wallet_type: "POSTPAID",
+        cycle_started_at: "01 JAN 25",
+        cycle_end_at: "31 FEB 25",
+        project_id: "IDW985W6-3W73-11WW-WW56-0242WW120002",
+        project_name: "test_project",
+        total_price: 2250000000,
+        vat: 2000000,
+        grand_total: 245000,
+        outstanding_balance: 30000000,
         resources: [
-          { name: 'Compute Instance', price: '100,000.00 THB' },
-          { name: 'Volumes', price: '50,000.00 THB' },
+          { name: "Compute Instance", price: 100000000 },
+          { name: "Volumes", price: 50000000 },
         ],
-        total_price: '225,000.00 THB',
-        vat: '15,750.00 THB',
-        grand_total: '240,750.00 THB',
       },
     };
     const response = await messageGatewayService.publishMessage(body);
